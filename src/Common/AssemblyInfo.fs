53f0dcf59bb3366e5de3c77d9a947fa49782f3de
Tomas Petricek <tomas@tomasp.net>
2013-11-28 01:54:43 +0000

    Update solution, update version

--- src/Common/AssemblyInfo.fs
+++ src/Common/AssemblyInfo.fs
@@ -1,12 +1,10 @@
-﻿namespace System
+﻿module internal AssemblyInfo
 open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyProductAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyDescriptionAttribute("A package for building great F# documentation, samples and blogs")>]
-[<assembly: AssemblyVersionAttribute("2.2.7")>]
-[<assembly: AssemblyFileVersionAttribute("2.2.7")>]
-do ()
+[<assembly: AssemblyVersionAttribute("2.2.9")>]
+[<assembly: AssemblyFileVersionAttribute("2.2.9")>]
 
-module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.2.7"
+()
