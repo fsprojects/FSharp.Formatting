8f2d7bd33138e087138b4c82cc8f2c6bf491614d
fbmnds <fbmnds7@gmail.com>
2013-12-31 15:25:26 +0100

    successful build after integration of 'fsformatting'

--- FSharp.FormattingCLI/Program.fs
+++
@@ -1,7 +0,0 @@
-﻿// Weitere Informationen zu F# unter "http://fsharp.net".
-// Weitere Hilfe finden Sie im Projekt "F#-Lernprogramm".
-
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // Exitcode aus ganzen Zahlen zurückgeben
---
+++ src/FSharp.FormattingCLI/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+﻿module AssemblyInfo
+open System.Reflection
+
+[<Literal>]
+let assemblyVersion = "2.2.10"
+
+[<assembly: AssemblyTitleAttribute("fsformatting")>]
+//[<assembly: AssemblyProductAttribute("FSharp.Formatting")>]
+[<assembly: AssemblyDescriptionAttribute("A commandline interface for FSharp.Formatting")>]
+[<assembly: AssemblyVersionAttribute(assemblyVersion)>]
+//[<assembly: AssemblyFileVersionAttribute("2.2.10")>]
+[<assembly: AssemblyCopyright("Apache 2.0 License")>] // added for Gsscoder/CommandLine
+
+()
+
