a740bbc043c5510c0dfe76142063395cfdc9c47e
Tomas Petricek <tomas@tomasp.net>
2014-01-19 20:30:27 +0100

    Update version info

--- src/Common/AssemblyInfo.fs
+++ src/Common/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyProductAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyDescriptionAttribute("A package for building great F# documentation, samples and blogs")>]
-[<assembly: AssemblyVersionAttribute("2.3.1")>]
-[<assembly: AssemblyFileVersionAttribute("2.3.1")>]
+[<assembly: AssemblyVersionAttribute("2.3.2")>]
+[<assembly: AssemblyFileVersionAttribute("2.3.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.3.1"
+    let [<Literal>] Version = "2.3.2"
