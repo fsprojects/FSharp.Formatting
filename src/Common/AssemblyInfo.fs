5bb4beba9e28cf7d77b313bce52e91622ba006bd
Tomas Petricek <tomas@tomasp.net>
2014-01-20 04:15:34 +0100

    Fix the dependency reference in NuGet package

--- src/Common/AssemblyInfo.fs
+++ src/Common/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyProductAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyDescriptionAttribute("A package of libraries for building great F# documentation, samples and blogs")>]
-[<assembly: AssemblyVersionAttribute("2.3.3")>]
-[<assembly: AssemblyFileVersionAttribute("2.3.3")>]
+[<assembly: AssemblyVersionAttribute("2.3.4")>]
+[<assembly: AssemblyFileVersionAttribute("2.3.4")>]
 [<assembly: AssemblyCopyrightAttribute("Apache 2.0 License")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.3.3"
+    let [<Literal>] Version = "2.3.4"
