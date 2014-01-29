be0cfd5804a65fab40398b18c4278031d30af627
John Palmer <j.palmer@physics.usyd.edu.au>
2014-01-30 10:15:01 +1100

    Fix case sensitivity issue in build.sh
    The script was testing for `Fake.exe` when the fake filename is
    `FAKE.exe`.  This made the build script very slow as it would reinstall
    fake every time.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -f packages/FAKE/tools/Fake.exe ]; then
+if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
   mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion -Prerelease
 fi
 mono packages/FAKE/tools/FAKE.exe build.fsx $@
