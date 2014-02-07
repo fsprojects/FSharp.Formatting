a77740ab28745fa9b820751da2fbacbb678958c1
Gustavo Guerra <gustavo@codebeside.org>
2014-02-07 01:19:15 +0000

    Fix properties displaying as methods (closes #114) + fix functions with unit input rendering incorrectly (closes #113)

--- src/FSharp.MetadataFormat/Main.fs
+++ src/FSharp.MetadataFormat/Main.fs
@@ -259,9 +259,14 @@ module ValueReader =
 
   // Format each argument, including its name and type 
   let formatArgUsage generateTypes i (arg:FSharpParameter) = 
+    let nm = 
+      match arg.Name with 
+      | null -> if arg.Type.NamedEntity.XmlDocSig = "T:Microsoft.FSharp.Core.unit"
+                then "()" 
+                else "arg" + string i 
+      | nm -> nm
     // Detect an optional argument 
     let isOptionalArg = hasAttrib<OptionalArgumentAttribute> arg.Attributes
-    let nm = match arg.Name with null -> "arg" + string i | nm -> nm
     let argName = if isOptionalArg then "?" + nm else nm
     if generateTypes then 
       (if String.IsNullOrWhiteSpace(arg.Name) then "" else argName + ":") + 
@@ -278,6 +283,7 @@ module ValueReader =
     |> List.map (List.map (fun x -> formatArgUsage generateTypes (counter()) x))
     |> List.map (function 
         | [] -> unit 
+        | [arg] when arg = unit -> unit
         | [arg] when not v.IsMember || isItemIndexer -> arg 
         | args when isItemIndexer -> String.concat tupSep args
         | args -> bracket (String.concat tupSep args))
@@ -335,6 +341,7 @@ module ValueReader =
     let signature =
       match argInfos with
       | [] -> retType
+      | [[x]] when v.IsGetterMethod && x.Name = null && x.Type.NamedEntity.XmlDocSig = "T:Microsoft.FSharp.Core.unit" -> retType
       | _  -> (formatArgsUsage true v argInfos) + " -> " + retType
 
     let usage = formatArgsUsage false v argInfos
