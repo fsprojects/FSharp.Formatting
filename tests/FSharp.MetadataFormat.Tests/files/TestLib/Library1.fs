314af1a30d54de46b45fe2a3f9ebe7ee08386e8b
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2014-02-23 14:42:09 -0500

    test library with xml documentation comments

---
+++ tests/FSharp.MetadataFormat.Tests/files/TestLib/Library1.fs
@@ -0,0 +1,21 @@
+﻿namespace FsLib
+
+/// <summary>
+/// Union sample
+/// </summary>
+type Union = 
+  /// Hello of int
+  | Hello of int
+  /// World of string and int
+  | World of string * int
+
+/// <summary>
+/// Record sample
+/// </summary>
+type Record = 
+  { /// This is name
+    Name : string
+    /// This is age
+    Age : int }
+  /// Additional member
+  member x.Foo = 0
