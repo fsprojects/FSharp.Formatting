5e586726a2754684eace0adf0cea59df63bb5797
Tomas Petricek <tomas@tomasp.net>
2014-04-25 14:28:12 +0100

    Ignore formatted tips in test

--- tests/FSharp.Literate.Tests/Tests.fs
+++ tests/FSharp.Literate.Tests/Tests.fs
@@ -130,8 +130,8 @@ let simpleMd = """
 
 [<Test>]
 let ``Parsing simple script and markdown produces the same result`` () =
-  let doc1 = Literate.ParseMarkdownString(simpleMd, formatAgent = getFormatAgent()) |> Literate.WriteHtml
-  let doc2 = Literate.ParseScriptString(simpleFsx, formatAgent = getFormatAgent()) |> Literate.WriteHtml
+  let doc1 = Literate.ParseMarkdownString(simpleMd, formatAgent = getFormatAgent()).With(formattedTips="") |> Literate.WriteHtml
+  let doc2 = Literate.ParseScriptString(simpleFsx, formatAgent = getFormatAgent()).With(formattedTips="") |> Literate.WriteHtml
   doc1 |> shouldEqual doc2
 
 // --------------------------------------------------------------------------------------
