c5edef30a9abf02aaa030d45d093c1e9756c88f1
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-19 19:32:06 +0200

    Use Paket

--- build.sh
+++ build.sh
@@ -2,10 +2,33 @@
 if test "$OS" = "Windows_NT"
 then
   # use .Net
-  .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  .paket/paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+
+  .paket/paket.exe install -v
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+
   packages/FAKE/tools/FAKE.exe build.fsx $@
 else
   # use mono
-  mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+
+  mono .paket/paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+
+  mono .paket/paket.exe install -v
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+  
   mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
