fabb221ec5e0797e983d476fb9413d1e95a07178
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-27 21:07:10 +0100

    oO, How can F# Formatting itself have no api reference :)

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -16,19 +16,26 @@ let template = source ++ "template.html"
 let templateSideBySide = source ++ "template-sidebyside.html"
 let sources = source ++ "../content"
 let output = source ++ "../output"
+let binDir = source ++ "../../bin"
 
 // When running locally, you can use your path
-//let root = @"file://C:\Tomas\Public\FSharp.Formatting\docs\output"
-let root = "http://tpetricek.github.io/FSharp.Formatting"
+let buildRoots = 
+  [ "github", "http://tpetricek.github.io/FSharp.Formatting"
+    "local", sprintf "file://%s" (Path.GetFullPath(output ++ "local")) ]
 
 // Additional strings to be replaced in the HTML template
 let projInfo =
   [ "page-author", "Tomas Petricek"
+    "project-author", "Tomas Petricek"
+    "project-nuget", "https://www.nuget.org/packages/FSharp.Formatting"
+    "project-github", "http://github.com/tpetricek/FSharp.Formatting"
     "page-description", "A package for building great F# documentation, samples and blogs"
     "github-link", "http://github.com/tpetricek/FSharp.Formatting"
-    "project-name", "F# Formatting"
-    "root", root ]
+    "project-name", "F# Formatting" ]
 
+// How RazorEngine searches templates
+let layoutRoots = 
+  [ source; source ++ "reference" ]
 // Compiler options (reference the two dll files and System.Web.dll)
 let options = 
   "--reference:\"" + source + "/../../bin/FSharp.CompilerBinding.dll\" " +
@@ -36,15 +43,22 @@ let options =
   "--reference:\"" + source + "/../../bin/FSharp.Markdown.dll\" " +
   "--reference:System.Web.dll"
 
-let generateDocs() =
+let generateDocs (outDirName, root) =
+  let projInfo = ("root", root) :: projInfo
+  let output = output ++ outDirName
   // Copy all sample data files to the "data" directory
   let copy = [ sources ++ "../files/misc", output ++ "misc"
                sources ++ "../files/content", output ++ "content" ]
+  let rec copyRecursive source dest =
+    Directory.CreateDirectory dest |> ignore
+    for f in Directory.EnumerateFiles source do
+      File.Copy(f, dest ++ Path.GetFileName f)
+    for d in Directory.EnumerateDirectories source do
+      copyRecursive d (dest ++ Path.GetFileName d)
+
   for source, target in copy do
     if Directory.Exists target then Directory.Delete(target, true)
-    Directory.CreateDirectory target |> ignore
-    for fileInfo in DirectoryInfo(source).EnumerateFiles() do
-        fileInfo.CopyTo(target ++ fileInfo.Name) |> ignore
+    copyRecursive source target
 
   // Now we can process the samples directory (with some additional references)
   // and then we clean up the files & directories we had to create earlier
@@ -73,6 +87,23 @@ let generateDocs() =
       printfn "Generating '%s'" outFile
       proc (source ++ ("../content/sidebyside/" + file)) (output ++ outFile) scriptInfo
 
+  // Create API reference
+  let binaries = 
+    [ "FSharp.CodeFormat.dll"; "FSharp.Literate.dll"; "FSharp.Markdown.dll"; "FSharp.MetadataFormat.dll" ]
+    |> Seq.map (fun b -> binDir ++ b)
+    |> Seq.toList
+  let referenceDir = Path.GetFullPath (output ++ "references")
+  if not <| Directory.Exists referenceDir then Directory.CreateDirectory referenceDir |> ignore
+  MetadataFormat.Generate
+    ( binaries, referenceDir, layoutRoots,
+      parameters = projInfo,
+      libDirs = [ binDir ],
+      //otherFlags = [],
+      sourceRepo = "https://github.com/tpetricek/FSharp.Formatting/blob/master/",
+      sourceFolder = "../../",
+      publicOnly = true,
+      markDownComments = false)
+      
 // Generate documentation
-generateDocs()
+buildRoots |> Seq.iter generateDocs
 
