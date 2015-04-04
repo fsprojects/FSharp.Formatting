f988c8088a3b52f6cd1f4b42dc2fdab600e058c5
Matthias Dittrich <matthi.d@googlemail.com>
2015-04-05 00:16:35 +0200

    load razor explicitly.

--- packages/FSharp.Formatting/FSharp.Formatting.fsx
+++ packages/FSharp.Formatting/FSharp.Formatting.fsx
@@ -2,15 +2,16 @@
 // Standard NuGet or Paket location
 #I "."
 #I "lib/net40"
+#r "./lib/net40/System.Web.Razor.dll"
 
 // Standard NuGet locations
-#I "../Microsoft.AspNet.Razor.3.2.3/lib/net45"
+#r "../Microsoft.AspNet.Razor.3.2.3/lib/net45/System.Web.Razor.dll"
 #I "../RazorEngine.3.6.4/lib/net45"
 #I "../FSharp.Compiler.Service.0.0.87/lib/net45"
 #I "../FSharpVSPowerTools.Core.1.8.0/lib/net45"
 
 // Standard Paket locations
-#I "../Microsoft.AspNet.Razor/lib/net45"
+#r "../Microsoft.AspNet.Razor/lib/net45/System.Web.Razor.dll"
 #I "../RazorEngine/lib/net45"
 #I "../FSharp.Compiler.Service/lib/net45"
 #I "../FSharpVSPowerTools.Core/lib/net45"
@@ -24,8 +25,6 @@
 
 // Reference VS PowerTools, Razor and F# Formatting components
 #r "FSharpVSPowerTools.Core.dll"
-#r "System.Web.Razor.dll"
-#r "RazorEngine.dll"
 #r "FSharp.Markdown.dll"
 #r "FSharp.Literate.dll"
 #r "FSharp.CodeFormat.dll"
