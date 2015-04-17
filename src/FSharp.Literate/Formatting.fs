2568f3747051944124e2381b57856191d56ac291
Colin <colinbul@googlemail.com>
2015-04-17 14:46:34 +0100

    Integrating WikiMedia formatting to Literate

--- src/FSharp.Literate/Formatting.fs
+++ src/FSharp.Literate/Formatting.fs
@@ -21,6 +21,11 @@ module internal Formatting =
         use wr = new StringWriter(sb)
         Html.formatMarkdown wr generateAnchors System.Environment.NewLine true doc.DefinedLinks doc.Paragraphs
         sb.ToString()
+    | OutputKind.WikiMedia -> 
+        let sb = new System.Text.StringBuilder()
+        use wr = new StringWriter(sb)
+        WikiMedia.formatMarkdown wr System.Environment.NewLine doc.DefinedLinks doc.Paragraphs
+        sb.ToString() 
 
   /// Try find first-level heading in the paragraph collection
   let findHeadings paragraphs generateAnchors (outputKind:OutputKind) =              
