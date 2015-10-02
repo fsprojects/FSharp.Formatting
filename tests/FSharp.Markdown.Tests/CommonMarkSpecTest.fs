af27c2efa3a7b851c9fe63208934d18024722953
Matthias Dittrich <matthi.d@googlemail.com>
2015-10-02 17:37:17 +0200

    use testcasesource instead of tt template.

--- tests/FSharp.Markdown.Tests/CommonMarkSpecTest.fs
+++ tests/FSharp.Markdown.Tests/CommonMarkSpecTest.fs
@@ -1,6 +1,14 @@
-﻿module MarkdownSpecificationParser.GenerateTestCasesFromSpecModule
+﻿module FSharp.Markdown.Tests.CommonMarkSpecTest
 
-let specHtmlFile = "tests/commonmark_spec.htm"
+open FsUnit
+open NUnit.Framework
+open FSharp.Markdown
+open System.IO
+open System.Diagnostics
+let (++) a b = Path.Combine(a, b)
+let testdir = __SOURCE_DIRECTORY__ ++ Path.Combine("..", "..", "tests")
+
+let specHtmlFile = testdir ++ "commonmark_spec.htm"
 let findTestCasesRegexString = """<pre><code class="language-markdown">(?<markdown>(((?!(>|<))(\w|\W|\s))|(<span class="space"> </span>))*)</code></pre>(\w|(?!(>|<))\W|\s)*<pre><code class="language-html">(?<html>(((?!(>|<))(\w|\W|\s))|(<span class="space"> </span>))*)</code></pre>"""
 let findTestCasesRegex = System.Text.RegularExpressions.Regex(findTestCasesRegexString)
 let getTestCases specHtmlFile =
@@ -19,5 +27,12 @@ let getTestCases specHtmlFile =
         yield normalizeString markdown, System.Net.WebUtility.HtmlDecode(normalizeString html)
   }
 
-let toFSharpStringLiteral (s:string) =
-  sprintf "\"%s\"" (s.Replace("\\", "\\\\").Replace("\"", "\\\"").Replace("\n", "\\n").Replace("\r", "\\r"))
+let getTest () =
+  getTestCases specHtmlFile
+  |> Seq.mapi (fun i (markdown, html) -> TestCaseData(i, markdown, html))
+
+[<Test>]
+[<TestCaseSource("getTest")>]
+let ``Run specification test`` (i : int) (markdown : string) (html : string) =
+  (Markdown.TransformHtml markdown).Replace("\r\n", "\n")
+  |> should equal html
--- tests/MarkdownSpecificationParser/Script.fsx
+++
@@ -1,8 +0,0 @@
-﻿// Weitere Informationen zu F# finden Sie unter http://fsharp.org. Im Projekt "F#-Lernprogramm" finden Sie
-// einen Leitfaden zum Programmieren in F#.
-
-#load "Library1.fs"
-open MarkdownSpecificationParser
-
-// Skriptcode für die Bibliothek hier definieren
-
