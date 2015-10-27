4b62f34a0c882f703231885683b35f179e2ba8ab
Limo Wan Kenobi <limowankenobi@gmail.com>
2015-10-27 17:33:06 +0100

    Adds Paket CSharpFormatter to not color URLs as comments in Paket files

---
+++ src/CSharpFormat/PaketFormat.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Manoli.Utils.CSharpFormat
+{
+	/// <summary>
+	/// Generates color-coded Paket source code.
+	/// </summary>
+	public class PaketFormat : FSharpFormat
+	{
+		/// <summary>
+		/// Regular expression string to match single line and multi-line 
+		/// comments (// and (* *)). Single line comments should not have 
+        /// a : before them to avoid color as comments URLs. For example
+        /// (source https://nuget.org/api/v2)
+		/// </summary>
+		protected override string CommentRegEx
+		{
+			get { return @"\(\*.*?\*\)|(?<!\:)//.*?(?=\r|\n)"; }
+		}
+	}
+}
