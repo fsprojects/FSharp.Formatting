cf1b35c7f22035b0bd182b32f18730ab055d146e
Limo Wan Kenobi <limowankenobi@gmail.com>
2015-10-27 17:39:56 +0100

    Untabify the WHOLE file

--- src/CSharpFormat/PaketFormat.cs
+++ src/CSharpFormat/PaketFormat.cs
@@ -7,17 +7,17 @@ namespace Manoli.Utils.CSharpFormat
     /// <summary>
     /// Generates color-coded Paket source code.
     /// </summary>
-	public class PaketFormat : FSharpFormat
+    public class PaketFormat : FSharpFormat
 	{
         /// <summary>
         /// Regular expression string to match single line and multi-line 
         /// comments (// and (* *)). Single line comments should not have 
         /// a : before them to avoid color as comments URLs. For example
         /// (source https://nuget.org/api/v2)
-		/// </summary>
-		protected override string CommentRegEx
-		{
-			get { return @"\(\*.*?\*\)|(?<!\:)//.*?(?=\r|\n)"; }
-		}
-	}
+        /// </summary>
+        protected override string CommentRegEx
+        {
+            get { return @"\(\*.*?\*\)|(?<!\:)//.*?(?=\r|\n)"; }
+        }
+    }
 }
