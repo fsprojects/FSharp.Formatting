2738af780074b8d7c0daf403be014bbf402cb4f0
Matthias Dittrich <matthi.d@gmail.com>
2015-12-30 00:35:29 +0100

    add development topics to the navigation links.

--- src/Common/AssemblyInfo.cs
+++ src/Common/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("FSharp.Formatting")]
 [assembly: AssemblyProductAttribute("FSharp.Formatting")]
 [assembly: AssemblyDescriptionAttribute("A package of libraries for building great F# documentation, samples and blogs")]
-[assembly: AssemblyVersionAttribute("2.12.1")]
-[assembly: AssemblyFileVersionAttribute("2.12.1")]
-[assembly: AssemblyInformationalVersionAttribute("2.12.1")]
+[assembly: AssemblyVersionAttribute("2.13.0")]
+[assembly: AssemblyFileVersionAttribute("2.13.0")]
+[assembly: AssemblyInformationalVersionAttribute("2.13.0")]
 [assembly: AssemblyCopyrightAttribute("Apache 2.0 License")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.12.1";
+        internal const string Version = "2.13.0";
     }
 }
