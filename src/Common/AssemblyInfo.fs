af60a64f86539c5143b90dd7d9e306b4318db462
Tomas Petricek <tomas@tomasp.net>
2016-04-05 11:18:32 +0100

    Release 2.14.1

--- src/Common/AssemblyInfo.fs
+++ src/Common/AssemblyInfo.fs
@@ -4,11 +4,11 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyProductAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyDescriptionAttribute("A package of libraries for building great F# documentation, samples and blogs")>]
-[<assembly: AssemblyVersionAttribute("2.13.6")>]
-[<assembly: AssemblyFileVersionAttribute("2.13.6")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.13.6")>]
+[<assembly: AssemblyVersionAttribute("2.14.1")>]
+[<assembly: AssemblyFileVersionAttribute("2.14.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.14.1")>]
 [<assembly: AssemblyCopyrightAttribute("Apache 2.0 License")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.13.6"
+    let [<Literal>] Version = "2.14.1"
