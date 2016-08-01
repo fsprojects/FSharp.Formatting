9087ca204ae6bb0e619d2cef9bd02056b2bfc5de
Tomas Petricek <tomas@tomasp.net>
2016-08-01 15:33:38 +0100

    Beta release

--- src/Common/AssemblyInfo.cs
+++ src/Common/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("FSharp.Formatting")]
 [assembly: AssemblyProductAttribute("FSharp.Formatting")]
 [assembly: AssemblyDescriptionAttribute("A package of libraries for building great F# documentation, samples and blogs")]
-[assembly: AssemblyVersionAttribute("2.14.3")]
-[assembly: AssemblyFileVersionAttribute("2.14.3")]
-[assembly: AssemblyInformationalVersionAttribute("2.14.3")]
+[assembly: AssemblyVersionAttribute("3.0.0")]
+[assembly: AssemblyFileVersionAttribute("3.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("3.0.0-beta01")]
 [assembly: AssemblyCopyrightAttribute("Apache 2.0 License")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.14.3";
+        internal const string Version = "3.0.0";
     }
 }
