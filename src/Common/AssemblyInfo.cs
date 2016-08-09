1120459f2d7d5f2b662390f20095c8f6835bf2dd
Rachel <rach@rachelree.se>
2016-08-09 12:48:05 -0400

    Re-adding fsx to .sln file.

--- src/Common/AssemblyInfo.cs
+++ src/Common/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("FSharp.Formatting")]
 [assembly: AssemblyProductAttribute("FSharp.Formatting")]
 [assembly: AssemblyDescriptionAttribute("A package of libraries for building great F# documentation, samples and blogs")]
-[assembly: AssemblyVersionAttribute("2.14.3")]
-[assembly: AssemblyFileVersionAttribute("2.14.3")]
-[assembly: AssemblyInformationalVersionAttribute("2.14.3")]
+[assembly: AssemblyVersionAttribute("2.14.4")]
+[assembly: AssemblyFileVersionAttribute("2.14.4")]
+[assembly: AssemblyInformationalVersionAttribute("2.14.4")]
 [assembly: AssemblyCopyrightAttribute("Apache 2.0 License")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.14.3";
-        internal const string InformationalVersion = "2.14.3";
+        internal const string Version = "2.14.4";
+        internal const string InformationalVersion = "2.14.4";
     }
 }
--- src/Common/AssemblyInfo.fs
+++ src/Common/AssemblyInfo.fs
@@ -4,12 +4,12 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyProductAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyDescriptionAttribute("A package of libraries for building great F# documentation, samples and blogs")>]
-[<assembly: AssemblyVersionAttribute("2.14.3")>]
-[<assembly: AssemblyFileVersionAttribute("2.14.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.14.3")>]
+[<assembly: AssemblyVersionAttribute("2.14.4")>]
+[<assembly: AssemblyFileVersionAttribute("2.14.4")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.14.4")>]
 [<assembly: AssemblyCopyrightAttribute("Apache 2.0 License")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.14.3"
-    let [<Literal>] InformationalVersion = "2.14.3"
+    let [<Literal>] Version = "2.14.4"
+    let [<Literal>] InformationalVersion = "2.14.4"
