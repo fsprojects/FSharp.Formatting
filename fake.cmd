e5901e73c65e33c9ebbe1145356691812101e10d
Matthias Dittrich <matthi.d@gmail.com>
2018-02-04 15:21:22 +0100

    2.1.4

--- fake.cmd
+++ fake.cmd
@@ -1,6 +1,6 @@
 
 REM Install .NET Core (https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script)
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -Channel Current"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -Channel 2.1.4"
 
 SET PATH=%LOCALAPPDATA%\Microsoft\dotnet;%PATH%
 dotnet restore dotnet-fake.csproj
