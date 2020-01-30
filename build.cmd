ca23840d8c98cff453a929721e1f93c3cd3433f4
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-01-30 11:48:52 +0000

    use dotnet sdk 3.0 tools

--- build.cmd
+++ build.cmd
@@ -1,11 +1,5 @@
 @echo off
-dotnet tool install fake-cli --tool-path .fake --version 5.12.6
 
-REM dotnet tool install paket --tool-path .paket
-
-.paket\paket.exe restore
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-.fake\fake.exe run build.fsx %*
+dotnet tool restore
+dotnet paket restore
+dotnet fake build %*
--- build.sh
+++ build.sh
@@ -1,16 +1,5 @@
-#!/bin/bash
-if test "$OS" = "Windows_NT"
-then
-  cmd /C build.cmd
-else
-  dotnet tool install fake-cli --tool-path .fake --version 5.12.6
-  # dotnet tool install paket --tool-path .paket
+#!bash
 
-  mono .paket/paket.exe restore
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-    exit $exit_code
-  fi
-  
- .fake/fake run build.fsx $@
-fi
+dotnet tool restore
+dotnet paket restore
+dotnet fake build "$@"
