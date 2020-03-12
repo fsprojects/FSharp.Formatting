3568ee2621da0e6ec304b2046cff49a13e9abd55
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-03-12 14:12:07 +0000

    fix git credentials for docker builds

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,7 @@ FROM eiriktsarpalis/dotnet-sdk-mono:3.1.101-buster
 WORKDIR /app
 COPY . .
 
+ENV GIT_ASKPASS=~/.git-askpass
+RUN echo 'echo $GITHUB_TOKEN' > ~/.git-askpass
+
 CMD ./build.sh
--- docker-build.sh
+++ docker-build.sh
@@ -9,6 +9,7 @@ docker build -t $IMAGE_LABEL .
 
 # dotnet build, test & nuget publish
 docker run -t --rm \
+           -e GITHUB_TOKEN=$GITHUB_TOKEN \
            -e NUGET_KEY=$NUGET_KEY \
            $IMAGE_LABEL \
            ./build.sh "$@"
