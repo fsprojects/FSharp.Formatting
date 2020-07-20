3564c5517dbbb54f3c41fbbca506d6140b354c5e
Don Syme <dsyme@microsoft.com>
2020-07-21 00:01:37 +0100

    version bump

--- src/Common/AssemblyInfo.cs
+++ src/Common/AssemblyInfo.cs
@@ -4,17 +4,17 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("FSharp.Formatting")]
 [assembly: AssemblyDescription("A package of libraries for building great F# documentation, samples and blogs")]
-[assembly: AssemblyVersion("6.0.6")]
-[assembly: AssemblyFileVersion("6.0.6")]
-[assembly: AssemblyInformationalVersion("6.0.6")]
+[assembly: AssemblyVersion("6.0.7")]
+[assembly: AssemblyFileVersion("6.0.7")]
+[assembly: AssemblyInformationalVersion("6.0.7")]
 [assembly: AssemblyCopyright("Apache 2.0 License")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyProduct = "FSharp.Formatting";
         internal const System.String AssemblyDescription = "A package of libraries for building great F# documentation, samples and blogs";
-        internal const System.String AssemblyVersion = "6.0.6";
-        internal const System.String AssemblyFileVersion = "6.0.6";
-        internal const System.String AssemblyInformationalVersion = "6.0.6";
+        internal const System.String AssemblyVersion = "6.0.7";
+        internal const System.String AssemblyFileVersion = "6.0.7";
+        internal const System.String AssemblyInformationalVersion = "6.0.7";
         internal const System.String AssemblyCopyright = "Apache 2.0 License";
     }
 }
