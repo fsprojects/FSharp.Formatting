590779aed064f7b3fc61cb079a03b0324fd70a3a
Don Syme <dsyme@microsoft.com>
2020-07-21 17:42:25 +0100

    release notes

--- src/Common/AssemblyInfo.fs
+++ src/Common/AssemblyInfo.fs
@@ -4,16 +4,16 @@ open System.Reflection
 
 [<assembly: AssemblyProductAttribute("FSharp.Formatting")>]
 [<assembly: AssemblyDescriptionAttribute("A package of libraries for building great F# documentation, samples and blogs")>]
-[<assembly: AssemblyVersionAttribute("6.0.7")>]
-[<assembly: AssemblyFileVersionAttribute("6.0.7")>]
-[<assembly: AssemblyInformationalVersionAttribute("6.0.7")>]
+[<assembly: AssemblyVersionAttribute("6.0.8")>]
+[<assembly: AssemblyFileVersionAttribute("6.0.8")>]
+[<assembly: AssemblyInformationalVersionAttribute("6.0.8")>]
 [<assembly: AssemblyCopyrightAttribute("Apache 2.0 License")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "FSharp.Formatting"
     let [<Literal>] AssemblyDescription = "A package of libraries for building great F# documentation, samples and blogs"
-    let [<Literal>] AssemblyVersion = "6.0.7"
-    let [<Literal>] AssemblyFileVersion = "6.0.7"
-    let [<Literal>] AssemblyInformationalVersion = "6.0.7"
+    let [<Literal>] AssemblyVersion = "6.0.8"
+    let [<Literal>] AssemblyFileVersion = "6.0.8"
+    let [<Literal>] AssemblyInformationalVersion = "6.0.8"
     let [<Literal>] AssemblyCopyright = "Apache 2.0 License"
