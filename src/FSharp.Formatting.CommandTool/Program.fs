0b2a1e3552b4f60642900b8be6c50bf12a0659e1
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-10-18 01:25:02 +0300

    Do not catch and then print exceptions at the entry point; let them be thrown.
    It is not needed. They will already be reported to the console and AggregateExceptions are not correctly handled.

--- src/FSharp.Formatting.CommandTool/Program.fs
+++ src/FSharp.Formatting.CommandTool/Program.fs
@@ -1,29 +1,11 @@
-
 module FSharp.Formatting.CommandTool.Main
 
 open CommandLine
 
-let printAssemblies msg =
-  printfn "%s. Loaded Assemblies:" msg
-  System.AppDomain.CurrentDomain.GetAssemblies()
-    |> Seq.choose (fun a -> try Some (a.GetName().FullName, a.Location) with _ -> None)
-    |> Seq.iter (fun (n, l) -> printfn "\t- %s: %s" n l)
-
-
 [<EntryPoint>]
 let main argv =
-    try
-      CommandLine.Parser.Default.ParseArguments(argv, typeof<BuildCommand>, typeof<WatchCommand>)
+    CommandLine.Parser.Default.ParseArguments<BuildCommand, WatchCommand>(argv)
         .MapResult(
             (fun (opts: BuildCommand) -> opts.Execute()),
             (fun (opts: WatchCommand) -> opts.Execute()),
-            (fun errs -> 1));
-    with e ->
-        let e =
-            match e with
-            | :? System.AggregateException as ex -> ex.InnerExceptions.[0]
-            | _ -> e
-        //printAssemblies "(DIAGNOSTICS) Documentation failed"
-        printfn "fsdocs.exe failed: %O" e
-        1
-        //reraise()
+            (fun _ -> 1))
