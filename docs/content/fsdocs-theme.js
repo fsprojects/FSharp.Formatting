c4eb60600dd9502ac37c388a90b6fd5f03e44933
nojaf <florian.verdonck@outlook.com>
2023-11-04 14:21:26 +0100

    Move 'On this page' section outside main element.

--- docs/content/fsdocs-theme.js
+++ docs/content/fsdocs-theme.js
@@ -1,5 +1,15 @@
 ﻿// Automatically scroll to the active aside menu item.
 const activeItem = document.querySelector("aside .nav-item.active");
-if (activeItem) {
-    activeItem.scrollIntoView();
+const mainMenu = document.getElementById('fsdocs-main-menu');
+if (activeItem && mainMenu) {
+    const halfMainMenuHeight = mainMenu.offsetHeight / 2
+    if(activeItem.offsetTop > halfMainMenuHeight){
+        mainMenu.scrollTop = (activeItem.offsetTop - halfMainMenuHeight) - (activeItem.offsetHeight / 2);
+    }
 }
+
+if(location.hash) {
+    const header = document.querySelector(`a[href='${location.hash}']`);
+    header.scrollIntoView({ behavior: 'instant'});
+}
+
